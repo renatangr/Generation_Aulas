95c7b36071cd940ca4316347261315cf6879a4ab
Renata Negrini <renatanegrinim@gmail.com>
2025-04-14 11:50:57 -0300

    :sparkles: Resolução do Exercício 3

---
+++ aula_02/src/aula_02/Exercicio3.java
@@ -0,0 +1,32 @@
+package aula_02;
+import java.util.Scanner;
+
+public class Exercicio3 {
+
+	public static void main(String[] args) {
+		/* Elabore um algoritmo que leia o Salário Bruto, o Adicional Noturno, as Horas Extras e 
+		 * os Descontos de um Colaborador e exiba na tela o Salário Líquido.
+		 * */
+		
+		float salarioBruto, adicionalNoturno, horasExtras, descontos, salarioLiquido;
+		
+		Scanner leia = new Scanner(System.in);
+		
+		System.out.print("Insira o salário bruto do colaborador: ");
+		salarioBruto = leia.nextFloat();
+		
+		System.out.print("Insira o adicional noturno do colaborador: ");
+		adicionalNoturno = leia.nextFloat();
+		
+		System.out.print("Insira as horas extras do colaborador: ");
+		horasExtras = leia.nextFloat();
+		
+		System.out.print("Insira os descontos do colaborador: ");
+		descontos = leia.nextFloat();
+		
+		salarioLiquido = salarioBruto + adicionalNoturno + (horasExtras * 5) - descontos;
+		
+		System.out.printf("Salário líquido do colaborador: %.2f", salarioLiquido);
+	}
+
+}
