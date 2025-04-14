c011d6a303b7d5b5e6e4fa051c2fdeb69ba62cf5
Renata Negrini <renatanegrinim@gmail.com>
2025-04-14 11:34:39 -0300

    Melhorias no Exercício 1

--- aula_02/src/aula_02/Exercicio1.java
+++ aula_02/src/aula_02/Exercicio1.java
@@ -7,14 +7,17 @@ public class Exercicio1 {
 		
 		// Digite o salário e o abono, realize a operação de soma e exiba o resultado.
 		Scanner leia = new Scanner(System.in);
+		float salario, abono, total;
 
 		System.out.print("Digite o valor do salário: ");
-		float salario = leia.nextFloat();
+		salario = leia.nextFloat();
 		
 		System.out.print("Digite o valor do abono: ");
-		float abono = leia.nextFloat();
+		abono = leia.nextFloat();
 		
-		System.out.printf("Novo salário: %.2f", (salario + abono));
+		total = salario + abono;
+		
+		System.out.printf("Novo salário: %.2f", total);
 		
 	}
 
