364c3ac5d7c2536347a108e33eb0fede31203b1d
Renata Negrini <renatanegrinim@gmail.com>
2025-04-14 12:10:54 -0300

    Adição da lógica que impede o cadastro de notas negativas

--- aula_02/src/aula_02/Exercicio2.java
+++ aula_02/src/aula_02/Exercicio2.java
@@ -12,17 +12,38 @@ public class Exercicio2 {
 		Scanner leia = new Scanner(System.in);
 		float nota1, nota2, nota3, nota4, media;
 		
+		do {
 		System.out.print("Digite o valor da Nota 1: ");
 		nota1 = leia.nextFloat();
+		if(nota1 < 0) {
+			System.out.println("Não existe nota menor que zero. Digite um valor adequado.");
+		}
+		} while(nota1 < 0);
 		
+		do {
 		System.out.print("Digite o valor da Nota 2: ");
 		nota2 = leia.nextFloat();
+		if(nota2 < 0) {
+			System.out.println("Não existe nota menor que zero. Digite um valor adequado.");
+		}
+		} while(nota2 < 0);
 		
+		do {
 		System.out.print("Digite o valor da Nota 3: ");
 		nota3 = leia.nextFloat();
+		if(nota3 < 0) {
+			System.out.println("Não existe nota menor que zero. Digite um valor adequado.");
+		}
+		} while(nota3 < 0);
 		
+		do {
 		System.out.print("Digite o valor da Nota 4: ");
 		nota4 = leia.nextFloat();
+		if(nota4 < 0) {
+			System.out.println("Não existe nota menor que zero. Digite um valor adequado.");
+		}
+		} while(nota4 < 0);
+		
 		
 		media = (nota1 + nota2 + nota3 + nota4)/ 4;
 		
