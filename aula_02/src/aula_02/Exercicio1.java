fe5093e7de86ebcd83db30b6df5c98a452c45e2b
Renata Negrini <renatanegrinim@gmail.com>
2025-04-14 12:05:14 -0300

    Lógica de verificação de salário para identificar valores menores que 1

--- aula_02/src/aula_02/Exercicio1.java
+++ aula_02/src/aula_02/Exercicio1.java
@@ -8,9 +8,11 @@ public class Exercicio1 {
 		// Digite o salário e o abono, realize a operação de soma e exiba o resultado.
 		Scanner leia = new Scanner(System.in);
 		float salario, abono, total;
-
+		
+		do {
 		System.out.print("Digite o valor do salário: ");
 		salario = leia.nextFloat();
+		} while (salario < 1);
 		
 		System.out.print("Digite o valor do abono: ");
 		abono = leia.nextFloat();
